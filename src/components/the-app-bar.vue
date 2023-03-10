2724b7704bab046e2b8d85f956436b8178e71ace
Alexander Titov <fox@raidorev.tech>
2023-03-11 00:32:40 +0600

    feat(app-bar): add app bar

---
+++ src/components/the-app-bar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import LocaleSwitcher from './locale-switcher.vue'
+import ThemeSwitcher from './theme-switcher.vue'
+</script>
+
+<template>
+  <v-app-bar color="teal-darken-4" image="@/assets/app-bar.jpg">
+    <template #image>
+      <v-img
+        gradient="to top right, rgba(125, 130, 184, .8), rgba(69, 176, 121, .8)"
+      />
+    </template>
+
+    <v-app-bar-title>Gather time!</v-app-bar-title>
+
+    <theme-switcher />
+    <locale-switcher />
+  </v-app-bar>
+</template>
--- src/layouts/default/view-layout.vue
+++ src/layouts/default/view-layout.vue
@@ -1,4 +1,10 @@
+<script lang="ts" setup>
+import TheAppBar from '@/components/the-app-bar.vue'
+</script>
+
 <template>
+  <the-app-bar />
+
   <v-main>
     <v-container>
       <router-view />
