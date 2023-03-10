0fc5596bcaeee39b9f87cafabf060ed495bf8696
Alexander Titov <fox@raidorev.tech>
2023-03-11 00:31:26 +0600

    feat(store): add "app" store

---
+++ src/store/app.ts
@@ -0,0 +1,22 @@
+import { defineStore } from 'pinia'
+import { reactive } from 'vue'
+
+export interface IApp {
+  theme: 'light' | 'dark'
+  locale: 'en' | 'ru'
+}
+
+export const useAppStore = defineStore(
+  'settings',
+  () => {
+    const app = reactive<IApp>({
+      theme: 'light',
+      locale: 'en',
+    })
+
+    return { app }
+  },
+  {
+    persist: true,
+  },
+)
