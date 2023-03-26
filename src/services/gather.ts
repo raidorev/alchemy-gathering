9af8ed2e166bb829fa9ee5884a5a9b46d2d404dc
Alexander Titov <fox@raidorev.tech>
2023-03-26 14:04:31 +0600

    refactor(gather): remove unused function

--- src/services/gather.ts
+++ src/services/gather.ts
@@ -1,4 +1,3 @@
-import { i18n } from '@/plugins/i18n'
 import { InventoryItem } from '@/services/inventory'
 import { Terrain } from '@/services/terrain'
 
@@ -14,27 +13,4 @@ export interface IGatherAttempt {
   terrain: Terrain
   roll: number
   items: InventoryItem[]
-}
-
-export const gatheringVariantName = (variant: GatheringVariant) => {
-  // TODO: Fix types
-  const { t } = i18n.global as unknown as { t: (key: string) => string }
-
-  switch (variant) {
-    case GatheringVariant.Flora: {
-      return t('gatheringVariant.flora')
-    }
-    case GatheringVariant.Essence: {
-      return t('gatheringVariant.essence')
-    }
-    case GatheringVariant.RareFlora: {
-      return t('gatheringVariant.rareFlora')
-    }
-    case GatheringVariant.RareFloraDifferentTerrain: {
-      return t('gatheringVariant.rareFloraDifferentTerrain')
-    }
-    default: {
-      throw new Error('Unknown variant')
-    }
-  }
 }
